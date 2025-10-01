c554c26048ee3882405f5b35dd07fe88eac352c7
Rooqx <ashfaga2005f@gmail.com>
2025-10-01 14:56:23 +0100

    updated the routes

--- server/src/services/job.service.ts
+++ server/src/services/job.service.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-// auth: {
-//           username: "sps",
-//           password: "caf5c8a29e40a997b05124e1ea68c445-us19", // Your actual API key
-//         },
+// a
+//            "sps",
+//
+//
 //https://remotive.com/api/remote-jobs this
 //https://remoteok.com/api
 //https://himalayas.app/jobs/api  this
@@ -13,17 +13,19 @@ class Job {
     try {
       /* const res = await axios.get("https://us19.api.mailchimp.com/3.0/", {
         headers: {
-          Authorization: `Bearer caf5c8a29e40a997b05124e1ea68c445-us19`,
+          Authorization: `Bearer `,
         },
       });
       console.log(res.statusText);*/
-      // return res.data; // return just the job data
+      // return res.data; // return just the job data caf5c8a29e40a997b05124e1ea68c445-us19
     } catch (err: unknown) {
       console.error("Failed to fetch:", (err as unknown).message);
       return null;
     }
   }
 }
+//"caf5c8a29e40a997b05124e1ea68c445
+// -us19",
 
 export default Job;
 
@@ -36,8 +38,7 @@ fetch("", {
 
 /**
  *   const res = await axios.get("https://api.getresponse.com/v3/contacts", {
-        headers: {
-          "X-Auth-Token": "api-key 0xfus124foucowgog6v825fpm4qtdo64",
+      0xfus124foucowgog6v825fpm4qtdo64",
         },
       });
  */
